7b0768a192f3684a14c73257dbbf18c72b2a73ba
MichaelCostaOliveira <37298354+MichaelCostaOliveira@users.noreply.github.com>
2018-06-08 13:09:30 -0300

    Update processa.php

--- crud/processa.php
+++ crud/processa.php
@@ -3,11 +3,13 @@ session_start();
 include_once("conexao.php");
 
 extract($_POST);
-//$resulta1= "SELECT crud_inserir (nome, email)";
-//$resultado1_usuario = mysqli_query($conn, $resultA1_usuario);
-//$retorn= mysqli_facht_array($resultado1_usuario)
 
-if(strlen($nome)>0 && strlen($email)>0 && strlen($senha)>0 /*&& $retorn=0*/){
+$select= "SELECT * FROM crud_inserir WHERE nome = '$nome' AND email ='$email'";
+$resultado_select = mysqli_query($conn, $select);
+$retorn= mysqli_num_rows($resultado_select);
+
+
+if(strlen($nome)>0 && strlen($email)>0 && strlen($senha)>0 && $retorn==0){
 	$result_usuario = "INSERT INTO crud_inserir (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 		$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
@@ -17,3 +19,27 @@ $resultado_usuario = mysqli_query($conn, $result_usuario);
 	header("Location: inserir.php");
 }
 
+
+
+
+
+
+/*<?php
+session_start();
+include_once("conexao.php");
+
+extract($_POST);
+//$resulta1= "SELECT crud_inserir (nome, email)";
+//$resultado1_usuario = mysqli_query($conn, $resultA1_usuario);
+//$retorn= mysqli_facht_array($resultado1_usuario)
+
+if(strlen($nome)>0 && strlen($email)>0 && strlen($senha)>0 /*&& $retorn=0*/){
+	/*$result_usuario = "INSERT INTO crud_inserir (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+		$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
+	header("Location: inserir.php");
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Preencha todos os campos!</p>";
+	header("Location: inserir.php");
+}
+*/
