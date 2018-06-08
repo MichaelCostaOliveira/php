31d2be0e346bd0264e28a8104ab851ecd90ab4f5
MichaelCostaOliveira <37298354+MichaelCostaOliveira@users.noreply.github.com>
2018-06-08 13:14:52 -0300

    Update processa.php

--- crud/processa.php
+++ crud/processa.php
@@ -8,8 +8,8 @@ $select= "SELECT * FROM crud_inserir WHERE nome = '$nome' AND email ='$email'";
 $resultado_select = mysqli_query($conn, $select);
 $retorn= mysqli_num_rows($resultado_select);
 
-
-if(strlen($nome)>0 && strlen($email)>0 && strlen($senha)>0 && $retorn==0){
+if($retorn==0){
+if(strlen($nome)>0 && strlen($email)>0 && strlen($senha)>0){
 	$result_usuario = "INSERT INTO crud_inserir (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 		$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
@@ -18,6 +18,11 @@ $resultado_usuario = mysqli_query($conn, $result_usuario);
 	$_SESSION['msg'] = "<p style='color:red;'>Preencha todos os campos!</p>";
 	header("Location: inserir.php");
 }
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário ja existente!</p>";
+	header("Location: inserir.php");
+}
+
 
 
 
