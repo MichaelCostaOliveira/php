4483587a66fd9d5c5e12cba01d5d8a61875b81c8
MichaelCostaOliveira <37298354+MichaelCostaOliveira@users.noreply.github.com>
2018-06-11 22:10:14 -0300

    Update inserir_produto.php

--- crud/inserir_produto.php
+++ crud/inserir_produto.php
@@ -6,8 +6,13 @@ session_start();
     include_once("conexao.php");
     
     extract($_POST);
-
-    $inserir_produto = "INSERT INTO produtos (nome, valor, imagem) VALUES ('$produto', '$valor', '$imagem')";
+	$caminho = "CRUD/crud/imagen/" . $imagem ;
+	$imagem2 = $caminho;
+	
+    $inserir_produto = "INSERT INTO produtos (nome, valor, imagem) VALUES ('$produto', '$valor', '$imagem2')";
     $insert = mysqli_query($conn, $inserir_produto);
     header("location: produtos.php");
+    /*$inserir_produto = "INSERT INTO produtos (nome, valor, imagem) VALUES ('$produto', '$valor', '$imagem')";
+    $insert = mysqli_query($conn, $inserir_produto);
+    header("location: produtos.php");*/
 ?>
