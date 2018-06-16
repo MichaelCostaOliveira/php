43d06fe5c55e66ccda2498400ac2d306ff310370
MichaelCostaOliveira <37298354+MichaelCostaOliveira@users.noreply.github.com>
2018-06-15 22:16:29 -0300

    Update inserir_produto.php

--- crud/inserir_produto.php
+++ crud/inserir_produto.php
@@ -1,3 +1,44 @@
+
+<?php
+session_start();
+    if(!$_SESSION['logado']/*=TRUE*/){
+        header('location: index.php');
+    }
+    include_once("conexao.php");
+    
+   extract($_POST);
+
+
+	 $imgBanco = $_FILES['imagem']['name'];
+    $caminhoPasta = "C:\\xampp\\htdocs\\CRUD\\crud\\imagen\\".$_FILES['imagem']['name'];
+   	
+
+  
+	 $i = move_uploaded_file($_FILES['imagem']['tmp_name'], $caminhoPasta );
+
+if($i == 1){
+
+
+
+ $inserir_produto = "INSERT INTO produtos(nome, valor, imagem) VALUES ('".$produto. "', '".$valor."','".$imgBanco."')";
+ $insert = mysqli_query($conn, $inserir_produto);
+  header("location: produtos.php");
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 <?php
 session_start();
     if(!$_SESSION['logado']/*=TRUE*/){
