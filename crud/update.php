5225db352fdd1a49bb7cc049ed650a8f8526f365
MichaelCostaOliveira <37298354+MichaelCostaOliveira@users.noreply.github.com>
2018-06-18 19:01:34 -0300

    Update update.php

--- crud/update.php
+++ crud/update.php
@@ -1,6 +1,40 @@
 <?php
 session_start();
 include_once("conexao.php");
+extract($_POST);
+echo "Nome: $nome <br>";
+echo "valor: $valor <br>";
+echo "imagem: $imagem <br>";
+echo "id : $id";
+
+	$imgBanco = $_FILES['imagem']['name'];
+    $caminhoPasta = "C:\\xampp\\htdocs\\CRUD\\crud\\imagen\\".$_FILES['imagem']['name'];
+
+	$update = "UPDATE produtos SET nome = '$nome', valor = '$valor', imagem = '$imagem' where id='$id'";
+	$resultado = mysqli_query($conn, $update);
+
+if(mysqli_affected_rows($conn)){
+    header("Location: produtos.php");
+}else{
+    header("Location: editar.php?id=$id");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*<?php
+session_start();
+include_once("conexao.php");
 
 extract($_POST);
 echo "Nome: $nome <br>";
@@ -14,4 +48,4 @@ if(mysqli_affected_rows($conn)){
     header("Location: produtos.php");
 }else{
     header("Location: editar.php?id=$id");
-}
+}*/
